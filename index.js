a143cb2fa5a1a7088e9be796a59513dc2c8ecce6
hachemn <hachemnourhen8@gmail.com>
2026-05-04 16:57:08 +0100

    break app for rollback test

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require("http");
 
 http.createServer((req, res) => {
-  res.end("Hello from InnoDeploy 🚀");
+  res.statusCode = 500; // 💣 simulate failure
+  res.end("broken");
 }).listen(3000);
